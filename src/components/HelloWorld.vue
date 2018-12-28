fb9d7b899923e9258deb36fda3d8a41f6f8050a9
adamb5441 <40124072+adamb5441@users.noreply.github.com>
2018-12-27 19:17:20 -0500

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+      <li v-for="stuff in list"
+      v-bind:list="stuff"
+      v-bind:key="stuff.id"
+      >
+      {{ stuff.item }}
+      <button  v-on:click="onClick(stuff.id)">delete</button>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    onClick: Function,
+    list: Array
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <input v-model="userIn"/>
+    <button  v-on:click="add">add</button>
+    <HelloWorld v-bind:onClick='remove' v-bind:list='val'/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  data: function() {
+    return {
+      val: [],
+      userIn: "",
+      count: 0
+      }
+  },
+  methods:{
+    add: function(){
+      this.val.push({id: this.count, item: this.userIn});
+      this.count++;
+    },
+    remove: function(index){
+      for(let i =0;i< this.val.length;i++){
+        if(this.val[i].id===index){
+          this.val.splice(i,1)
+        }
+      }
+    }
+  }
+}
+</script>
