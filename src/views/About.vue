169f94e52c4bba30e9dd9f8b01628b0a55f35d6d
adamb5441 <40124072+adamb5441@users.noreply.github.com>
2018-12-28 21:47:52 -0500

    I got nested routes to work

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+          <router-link to="/about/">profile</router-link> |
+        <router-link to="/about/posts">posts</router-link>
+              <router-view></router-view>
   </div>
 </template>
+
---
+++ src/views/UserPosts.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="UserPost">
+    <h1>post page</h1>
+  </div>
+</template>
+
